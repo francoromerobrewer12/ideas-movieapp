d7a295d327a40871c8af4ed11114dae5571f55e6
francoromerobrewer <francoromerobrewer12@gmail.com>
2022-03-02 11:27:01 -0500

    Creating theme to change some buttons properties in main page and remove unnecessary menu options

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -12,103 +12,6 @@ import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <FontAwesomeIcon icon={faBell} />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <FontAwesomeIcon icon={faBell} />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <FontAwesomeIcon icon={faBell} />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
-
   return (
     <Box sx={{ flexGrow: 1, mt: 5 }}>
       <AppBar
@@ -124,17 +27,6 @@ function Navbar() {
           >
             Popflixs
           </Typography>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{
-              display: { xs: "none", sm: "block", fontFamily: "Bebas Neue" },
-              mr: 2,
-            }}
-          >
-            Add Movie
-          </Typography>
 
           <Box sx={{ flexGrow: 1 }} />
           <Box>
@@ -143,29 +35,20 @@ function Navbar() {
               edge="start"
               color="inherit"
               aria-label="open drawer"
-              onClick={handleMobileMenuOpen}
             >
-              <FontAwesomeIcon icon={faBarsStaggered} />
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <FontAwesomeIcon icon={faBell} />
-              </Badge>
+              <FontAwesomeIcon icon={faBarsStaggered} color="#66ffb3" />
             </IconButton>
           </Box>
           <Box sx={{ ml: 1 }}>
-            <IconButton onClick={handleMobileMenuOpen} sx={{ p: 0 }}>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            <IconButton sx={{ p: 0 }}>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://www.mdcleaners.com/wp-content/uploads/2016/11/nouser-1.jpg"
+              />
             </IconButton>
           </Box>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 }
